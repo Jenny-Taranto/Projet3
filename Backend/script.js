ee3871b6e9a745f3dc42f88f15b9c5029459cc32
Jenny-Taranto <jennytaranto.pro@gmail.com>
2024-12-27 10:10:53 +0100

    Add files via upload

---
+++ Backend/script.js
@@ -0,0 +1,8 @@
+const works = await fetch("http://localhost:5678/api/works");
+
+
+for (let i = 0; i < works.length; i++) {
+const gallery = `
+<img src="http://localhost:5678/images/abajour-tahina1651286843956.png">
+`
+}
