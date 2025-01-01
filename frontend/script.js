d7c6529de81a94e6fb3ce6db911e7191015e8ab6
Jenny Taranto <jennytaranto.pro@gmail.com>
2025-01-01 18:05:53 +0100

    création barre des filtres terminée

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,20 @@
+// Requête serveur pour récupérer les catégories //
+
+async function fetchcategories() {
+  try {
+    const response = await fetch("http://localhost:5678/api/categories");
+
+    if (!response.ok) {
+      throw new Error(`Erreur HTTP : ${response.status}`);
+    }
+
+    const categories = await response.json(); // Les données récupérées
+    genererCategories(categories); // Appel de la fonction pour afficher les données
+  } catch (error) {
+    console.error("Erreur lors de la récupération des données :", error);
+  }
+}
+
 // Requête serveur pour récupérer les "travaux" //
 
 async function fetchworks() {
@@ -15,6 +32,32 @@ async function fetchworks() {
     }
   }
 
+// Génération des catégories //
+
+function genererCategories(categories) {
+  const barreDesFiltres = document.getElementById("filterbar");
+
+    // Création du bouton "Tous" //
+    const btnFiltreTous = document.createElement("button");
+    btnFiltreTous.textContent = "Tous";
+    barreDesFiltres.appendChild(btnFiltreTous);
+
+    // Génération des boutons pour chaque catégorie //
+    for (let i = 0; i < categories.length; i++) {
+
+    const categorie = categories[i];
+
+    // Création des autres boutons //
+
+    const btnFiltre = document.createElement("button");
+    btnFiltre.textContent = categorie.name;
+
+    barreDesFiltres.appendChild(btnFiltre);
+    }
+  };
+
+
+fetchcategories();
 
 // Génération de la liste des travaux //
 
@@ -49,20 +92,3 @@ function genererTravaux(works){
 fetchworks();
 
 
-//Génération des filtres //
-
-const barreDesFiltres = document.getElementById("filterbar");
-
-const boutonFiltre = [
-  { id: 1, text: "Tous"},
-  { id: 2, text: "Objets"},
-  { id: 3, text: "Appartements"},
-  { id: 4, text: "Hotels & restaurants"},
-]
-
-for (const bouton of boutonFiltre) {
-  const btn = document.createElement("button");
-  btn.textContent = bouton.text;
-  btn.setAttribute("data-id", bouton.id);
-  barreDesFiltres.appendChild(btn);
-}
